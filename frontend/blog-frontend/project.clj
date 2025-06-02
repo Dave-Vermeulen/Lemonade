fb95bc79bdd7d29e892f5c2a9a11a158633a2162
Dave-Vermeulen <davver911@gmail.com>
2025-06-02 22:06:33 +0200

    debug frontend

--- frontend/blog-frontend/project.clj
+++ frontend/blog-frontend/project.clj
@@ -1,6 +1,6 @@
 (defproject blog-frontend "0.1.0-SNAPSHOT"
-  :description "FIXME: write this!"
-  :url "http://example.com/FIXME"
+  :description "Retro Clojure Blog Frontend"
+  :url "http://localhost:3449"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
@@ -8,91 +8,61 @@
 
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/clojurescript "1.11.60"]
-                 [org.clojure/core.async  "0.4.500"]
+                 [org.clojure/core.async "0.4.500"]
                  [reagent "1.1.0"]
                  [cljs-http "0.1.46"]
                  [garden "1.3.10"]]
 
   :plugins [[lein-figwheel "0.5.20"]
-            [lein-cljsbuild "1.1.7" :exclusions [[org.clojure/clojure]]]]
+            [lein-cljsbuild "1.1.7" :exclusions [[org.clojure/clojure]]]
+            [lein-npm "0.6.2"]]
 
-  :source-paths ["src"]
+  :npm {:dependencies [[react "17.0.2"]
+                       [react-dom "17.0.2"]
+                       ["@cljs-oss/module-deps" "1.1.0"]]}
 
-  :cljsbuild {:builds
-              [{:id "dev"
-                :source-paths ["src"]
+  :source-paths ["src" "dev"]
 
-                ;; The presence of a :figwheel configuration here
-                ;; will cause figwheel to inject the figwheel client
-                ;; into your build
-                :figwheel {:on-jsload "blog-frontend.core/on-js-reload"
-                           ;; :open-urls will pop open your application
-                           ;; in the default browser once Figwheel has
-                           ;; started and compiled your application.
-                           ;; Comment this out once it no longer serves you.
-                           :open-urls ["http://localhost:3449/index.html"]}
+  :cljsbuild {
+    :builds [
+      {
+        :id "dev"
+        :source-paths ["src" "dev"]
+        :figwheel {
+          :on-jsload "blog-frontend.core/on-js-reload"
+          :open-urls ["http://localhost:3449/index.html"]
+        }
+        :compiler {
+          :main blog-frontend.core
+          :asset-path "js/compiled/out"
+          :output-to "resources/public/js/compiled/blog_frontend.js"
+          :output-dir "resources/public/js/compiled/out"
+          :source-map-timestamp true
+          :preloads [devtools.preload]
+          :install-deps true
+          :npm-deps true
+          :infer-externs true
+          :closure-warnings {:non-standard-jsdoc :off}
+        }
+      }
+    ]
+  }
 
-                :compiler {:main blog-frontend.core
-                           :asset-path "js/compiled/out"
-                           :output-to "resources/public/js/compiled/blog_frontend.js"
-                           :output-dir "resources/public/js/compiled/out"
-                           :source-map-timestamp true
-                           ;; To console.log CLJS data-structures make sure you enable devtools in Chrome
-                           ;; https://github.com/binaryage/cljs-devtools
-                           :preloads [devtools.preload]}}
-               ;; This next build is a compressed minified build for
-               ;; production. You can build this with:
-               ;; lein cljsbuild once min
-               {:id "min"
-                :source-paths ["src"]
-                :compiler {:output-to "resources/public/js/compiled/blog_frontend.js"
-                           :main blog-frontend.core
-                           :optimizations :advanced
-                           :pretty-print false}}]}
+  :figwheel {
+    :css-dirs ["resources/public/css"]
+  }
 
-  :figwheel {;; :http-server-root "public" ;; default and assumes "resources"
-             ;; :server-port 3449 ;; default
-             ;; :server-ip "127.0.0.1"
-
-             :css-dirs ["resources/public/css"] ;; watch and update CSS
-
-             ;; Start an nREPL server into the running figwheel process
-             ;; :nrepl-port 7888
-
-             ;; Server Ring Handler (optional)
-             ;; if you want to embed a ring handler into the figwheel http-kit
-             ;; server, this is for simple ring servers, if this
-
-             ;; doesn't work for you just run your own server :) (see lein-ring)
-
-             ;; :ring-handler hello_world.server/handler
-
-             ;; To be able to open files in your editor from the heads up display
-             ;; you will need to put a script on your path.
-             ;; that script will have to take a file path and a line number
-             ;; ie. in  ~/bin/myfile-opener
-             ;; #! /bin/sh
-             ;; emacsclient -n +$2 $1
-             ;;
-             ;; :open-file-command "myfile-opener"
-
-             ;; if you are using emacsclient you can just use
-             ;; :open-file-command "emacsclient"
-
-             ;; if you want to disable the REPL
-             ;; :repl false
-
-             ;; to configure a different figwheel logfile path
-             ;; :server-logfile "tmp/logs/figwheel-logfile.log"
-
-             ;; to pipe all the output to the repl
-             ;; :server-logfile false
-             }
-
-  :profiles {:dev {:dependencies [[binaryage/devtools "1.0.0"]
-                                  [figwheel-sidecar "0.5.20"]]
-                   ;; need to add dev source path here to get user.clj loaded
-                   :source-paths ["src" "dev"]
-                   ;; need to add the compiled assets to the :clean-targets
-                   :clean-targets ^{:protect false} ["resources/public/js/compiled"
-                                                     :target-path]}})
+  :profiles {
+    :dev {
+      :dependencies [
+        [binaryage/devtools "1.0.0"]
+        [figwheel-sidecar "0.5.20"]
+      ]
+      :source-paths ["src" "dev"]
+      :clean-targets ^{:protect false} [
+        "resources/public/js/compiled"
+        :target-path
+      ]
+    }
+  }
+)
--- frontend/blog-frontend/src/blog_frontend/core.cljs
+++ frontend/blog-frontend/src/blog_frontend/core.cljs
@@ -1,29 +1,58 @@
 (ns blog-frontend.core
   (:require [reagent.core :as r]
-            [cljs-http.client :as http]))
+            [cljs-http.client :as http]
+            [clojure.string :as str]))
 
-(defonce state (r/atom {:posts []}))
+(defonce state (r/atom {:posts []
+                        :loading? true
+                        :error nil}))
 
 (defn fetch-posts []
+  (swap! state assoc :loading? true :error nil)
   (http/get "http://localhost:3000/posts"
             {:with-credentials? false
-             :json-params {}
-             :handler #(reset! state {:posts (:body %)})
-             :error-handler #(js/console.error "Failed to fetch posts:" %)}))
+             :headers {"Accept" "application/json"}
+             :handler (fn [response]
+                        (swap! state assoc 
+                               :posts (:body response)
+                               :loading? false))
+             :error-handler (fn [error]
+                              (swap! state assoc 
+                                     :error (str "Failed to fetch posts: " (:status error))
+                                     :loading? false))}))
 
 (defn post-component [post]
-  [:div {:class "post"}
-   [:h2 (:title post)]
-   [:p (:content post)]
-   [:small (str "Tags: " (clojure.string/join ", " (:tags post)))]])
+  [:div {:class "post retro-box"}
+   [:h2 {:class "retro-title"} (:title post)]
+   [:div {:class "retro-meta"}
+    [:span {:class "retro-date"} (-> (:published_at post) (str/split #"T") first)]
+    [:span {:class "retro-tags"} 
+     (str "Tags: " (str/join ", " (:tags post)))]
+   [:p {:class "retro-content"} (:content post)]
+   [:div {:class "retro-footer"} 
+    [:a {:href (str "/post/" (:slug post))} "Read more →"]]]])
 
 (defn blog-view []
-  (fetch-posts)
-  (fn []
-    [:div
-     [:h1 "🚀 Welcome to My Retro Blog"]
-     (for [post (:posts @state)]
-       [post-component post])]))
+  (r/create-class
+   {:component-did-mount fetch-posts  ; Fetch posts on mount
+    :reagent-render
+    (fn []
+      [:div {:class "retro-container"}
+       [:header {:class "retro-header"}
+        [:h1 "🚀 Welcome to My Retro Blog"]
+        [:p "A blast from the past with Clojure power!"]]
+       
+       (cond
+         (:loading? @state) 
+         [:div {:class "retro-loading"} "⌛ Loading posts..."]
+         
+         (:error @state) 
+         [:div {:class "retro-error"} "❌ " (:error @state)]
+         
+         :else
+         [:div {:class "retro-posts"}
+          (for [post (:posts @state)]
+            ^{:key (:id post)} [post-component post])])])}))
 
 (defn ^:export main []
   (r/render [blog-view] (.getElementById js/document "app")))
